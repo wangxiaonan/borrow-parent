58041089d43e4a565a4c49f7af14a4b77e3b5d70
wangxiaonan <xiaonan11@staff.sina.com.cn>
2019-05-21 17:17:58 +0800

    添加图片

---
+++ borrow-manage/src/main/java/com/borrow/manage/config/ImageConfig.java
@@ -0,0 +1,29 @@
+package com.borrow.manage.config;
+
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.MultipartConfigElement;
+
+/**
+ * Created by wxn on 2019-05-21
+ */
+@Configuration
+public class ImageConfig {
+
+    /**
+     * 文件上传配置
+     * @return
+     */
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //文件最大
+        factory.setMaxFileSize("10240KB"); //KB,MB
+        /// 设置总上传数据总大小
+        factory.setMaxRequestSize("102400KB");
+        return factory.createMultipartConfig();
+    }
+
+}
