4f79bdf6008e3e020a1427abedee3c5bc1bc8969
wangxiaonan <xiaonan11@staff.sina.com.cn>
2019-12-26 19:17:27 +0800

    添加修复sql

---
+++ borrow-manage/sql/理财端完成回款修复1226.sql
@@ -0,0 +1,9 @@
+update borrow_repayment set repay_status = 2,repay_type = 1 where order_id = 134854841301204992;
+update borrow_order set bo_is_state = 6, bo_pay_expect = bo_expect where order_id = 134854841301204992;
+
+update borrow_repayment set repay_status = 2,repay_type = 1 where order_id = 163154131706445824;
+update borrow_order set bo_is_state = 6, bo_pay_expect = bo_expect where order_id = 163154131706445824;
+
+
+update borrow_repayment set repay_status = 2,repay_type = 1 where order_id = 156660650108518400;
+update borrow_order set bo_is_state = 6, bo_pay_expect = bo_expect where order_id = 156660650108518400;
